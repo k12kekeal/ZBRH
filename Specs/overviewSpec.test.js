6468649cfea0df576df5be991f072dfcdc1e583a
Kela Kealakai <k12kekeal@gmail.com>
2020-11-13 10:11:45 -0600

    Deleted unused folders from old project, added requests for Overview, added user stories to spec

--- Specs/overviewSpec.test.js
+++ Specs/overviewSpec.test.js
@@ -1,1 +1,43 @@
-//Overview tests go here
+/* PRODUCT INFORMATION
+
+ >>>Star Rating<<<
+A total of 5 stars should always appear
+The number of stars filled in should correspond to the average score
+The visual rating should be representative of up to a quarter of a review point
+There should be a link next to the star rating stating "Read all [#] reviews"
+[#] should be replaced with the total number of reviews
+Clicking this link should scroll the page to the Ratings & Reviews module
+If there are no views, this entire section should be hidden
+
+ >>>Product Category<<<
+
+ >>>Product Title<<<
+
+ >>>Price<<<
+Price should dynamically updated with the user's updates to style selected
+A default style should be designated for each product
+A default style should appear if no further selection is made
+If the SKU is currently discounted, the sale price should appear in red, followed by the original price, which is struckthrough
+
+ >>>Product Overview<<<
+If available, should be displayed
+
+ >>>Share on Social Media<<<
+Below product information, should provide ability to share this product on FB, Twitter, Pinterest
+
+ STYLE SELECTOR
+
+ ADD TO CART
+
+ >>>Size Selector<<<
+
+ >>>Quantity Selector<<<
+
+ >>>Add to Cart<<<
+
+ IMAGE GALLERY
+
+ >>>Default View<<<
+
+ >>>Expanded View<<<
+ */
