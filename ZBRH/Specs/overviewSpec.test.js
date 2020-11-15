f296c5654b0bbe9dfa08f36141dde8f63fadac08
Kela Kealakai <k12kekeal@gmail.com>
2020-11-15 17:56:39 -0600

    Added star svg, installed material-ui, used material ui Rating

--- ZBRH/Specs/overviewSpec.test.js
+++ ZBRH/Specs/overviewSpec.test.js
@@ -1,4 +1,11 @@
 
+
+const sum = require('ZBRH/react-client/src/sum.js');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
 TODO:// Fill in tests, finish writing tests for later features
 
 // PRODUCT INFORMATION
--- ZBRH/react-client/src/components/Overview/Overview.jsx
+++ ZBRH/react-client/src/components/Overview/Overview.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
+import Rating from '@material-ui/lab/Rating';
 
-class Overview extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>Hi </div>
-    );
-  }
-}
+const Overview = () => {
+  return (
+    <div>
+      <h3>Hi</h3>
+      <div>
+        <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly/>
+      </div>
+    </div>
+  );
+};
 
 export default Overview;
