7dbec658886c44b43db98b9fd4fd75788c5429c9
Kela Kealakai <k12kekeal@gmail.com>
2020-11-16 10:34:13 -0600

    Added star rating component to Overview

--- ZBRH/react-client/src/components/Overview/Overview.jsx
+++ ZBRH/react-client/src/components/Overview/Overview.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Rating from '@material-ui/lab/Rating';
 
-const Overview = () => {
+const Overview = ({value}) => {
   return (
     <div>
       <h3>Hi</h3>
       <div>
-        <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly/>
+        <Rating name="half-rating" defaultValue={2.5} precision={0.25} />
       </div>
     </div>
   );
