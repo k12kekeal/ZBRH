d1af3b050ae34872babd2fa3385929d49addd1e9
Kela Kealakai <k12kekeal@gmail.com>
2020-11-18 11:34:03 -0600

    Added conditional rendering for all elements of ProductOverview

--- ZBRH/react-client/src/components/Overview/ProductOverview.jsx
+++ ZBRH/react-client/src/components/Overview/ProductOverview.jsx
@@ -4,8 +4,12 @@ const ProductOverview = ({ currentProduct }) => {
   return (
     <div>
       {currentProduct.slogan ? <h3>{currentProduct.slogan}</h3> : null}
+
       {currentProduct.description ? <p>{currentProduct.description}</p> : null}
-      {/* <div>{currentProduct.features}</div> */}
+
+      {currentProduct.features ? currentProduct.features.map(feature => (
+        <ul>{feature.feature}: {feature.value}</ul>
+      )) : null}
     </div>
   );
 };
